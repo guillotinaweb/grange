cacbabe29a13d79ff22f655772f7f34afd369d83
Eric BREHAULT <ebrehault@gmail.com>
2020-05-28 08:57:09 +0200

    fix updateContext and add onComplete

--- projects/grange/src/lib/grange.service.ts
+++ projects/grange/src/lib/grange.service.ts
@@ -4,9 +4,9 @@ import { Traverser } from 'angular-traversal';
 import { GrangeCore } from '@guillotinaweb/grange-core';
 import { GrangeState } from './state/state';
 import { PastanagaService } from '@guillotinaweb/pastanaga-angular';
-import { Observable } from 'rxjs';
+import { Observable, AsyncSubject } from 'rxjs';
 import { TraverserSelectors, TraverserActions } from '@guillotinaweb/ngx-state-traverser';
-import { take } from 'rxjs/operators';
+import { take, concatMap, tap, skip } from 'rxjs/operators';
 
 @Injectable({
     providedIn: 'root'
@@ -26,12 +26,29 @@ export class Grange {
         return this.store.pipe(select(TraverserSelectors.getContext));
     }
 
-    updateContext(changes: any) {
+    updateContext(changes: any): {onComplete: Observable<boolean>} {
+        const onComplete = new AsyncSubject<boolean>();
+        let initialContext: any;
+        let path: string;
         this.getContext().pipe(
-            take(1)
-        ).subscribe(context => this.store.dispatch(new TraverserActions.UpdateTraverserResource({
-            path: this.core.api.getPath(context['@id']),
-            changes
-        })));
+            take(1),
+            tap(context => {
+                initialContext = context;
+                path = this.core.api.getPath(context['@id']);
+                this.store.dispatch(new TraverserActions.UpdateTraverserResource({path, changes}));
+            }),
+            concatMap(newContext => this.core.resource.update(path, changes)),
+        ).subscribe(
+            () => {
+                onComplete.next(true);
+                onComplete.complete();
+            },
+            () => {
+                this.store.dispatch(new TraverserActions.UpdateTraverserResource({path, changes: initialContext}));
+                onComplete.next(false);
+                onComplete.complete();
+            },
+        );
+        return {onComplete};
     }
 }
--- projects/grange/src/lib/state/effects.ts
+++ projects/grange/src/lib/state/effects.ts
@@ -1,11 +1,10 @@
 import { Injectable } from '@angular/core';
 import { Actions, Effect, ofType } from '@ngrx/effects';
-import { tap, switchMap, take, concatMap, filter, catchError, map } from 'rxjs/operators';
-import { TraverserActions, TraverserSelectors } from '@guillotinaweb/ngx-state-traverser';
-import { select, Store } from '@ngrx/store';
+import { tap } from 'rxjs/operators';
+import { TraverserActions } from '@guillotinaweb/ngx-state-traverser';
+import { Store } from '@ngrx/store';
 import { GrangeState } from './state';
 import { GrangeCore } from '@guillotinaweb/grange-core';
-import { EMPTY, of } from 'rxjs';
 
 @Injectable()
 export class GrangeEffects {
@@ -14,21 +13,6 @@ export class GrangeEffects {
     readonly onTraverse = this.actions.pipe(
         ofType(TraverserActions.Types.Traverse),
         tap(() => window.scrollTo(0, 0))
-    );
-
-    @Effect()
-    updateResource = this.actions.pipe(
-        ofType<TraverserActions.UpdateTraverserResource>(TraverserActions.Types.UpdateTraverserResource),
-        switchMap(action => this.store.pipe(
-            select(TraverserSelectors.getObjectByPath(action.payload.path)),
-            take(1),
-            filter(resource => !!resource['@id']),
-            concatMap(resource => this.core.resource.update(resource['@id'], resource).pipe(
-                map(() => EMPTY),
-                // if error on update, we traverse the resource in order to put the backend version in our local state
-                catchError(() => of(new TraverserActions.Traverse(this.core.api.getPath(resource['@id']))))
-            )),
-        )),
     );
 
     constructor(
