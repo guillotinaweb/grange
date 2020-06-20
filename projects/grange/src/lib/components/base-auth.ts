990eedd798dbc0826c1cbcddd3b5c9c8738eaf2c
Eric BREHAULT <ebrehault@gmail.com>
2020-06-20 15:27:11 +0200

    use config via grange + hide social login is no option

--- projects/grange/src/lib/components/base-auth.ts
+++ projects/grange/src/lib/components/base-auth.ts
@@ -1,25 +1,21 @@
 import { Grange } from '../grange.service';
-import { ConfigurationService } from '@guillotinaweb/grange-core';
-
-
 import { Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
 import { markForCheck } from '@guillotinaweb/pastanaga-angular';
 
 @Component({
-  selector: 'grange-auth',
-  templateUrl: './base-auth.html',
-  styleUrls: ['./base-auth.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
+    selector: 'grange-auth',
+    templateUrl: './base-auth.html',
+    styleUrls: ['./base-auth.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class BaseAuthComponent implements OnInit {
     logo?: string;
 
     constructor(
         public grange: Grange,
-        protected config: ConfigurationService,
         private cdr: ChangeDetectorRef
     ) {
-      this.logo = this.config.get('LOGO');
+        this.logo = this.grange.core.config.get('LOGO');
     }
 
     ngOnInit() {
--- projects/grange/src/lib/views/user/login.ts
+++ projects/grange/src/lib/views/user/login.ts
@@ -1,77 +1,77 @@
 import { Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
 import { Grange } from '../../grange.service';
 import { LoginInfo } from '@guillotinaweb/grange-core';
-import { ConfigurationService } from '@guillotinaweb/grange-core';
 import { markForCheck } from '@guillotinaweb/pastanaga-angular';
 
 @Component({
-  selector: 'grange-login',
-  templateUrl: './login.html',
-  styleUrls: ['./login.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
+    selector: 'grange-login',
+    templateUrl: './login.html',
+    styleUrls: ['./login.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LoginView implements OnInit {
-  useToken = false;
-  error = '';
-  isLogged: boolean;
-  title?: string;
-  allowRegister = false;
-  allowSocial = false;
-  allowForgot = false;
-  allowToken = false;
-  providers = [];
+    useToken = false;
+    error = '';
+    isLogged: boolean;
+    title?: string;
+    allowRegister = false;
+    allowSocial = false;
+    allowForgot = false;
+    allowToken = false;
+    providers = [];
+    hasSocialLogin = false;
 
-  constructor(
-    public grange: Grange,
-    protected config: ConfigurationService,
-    private cdr: ChangeDetectorRef
-  ) {
-    this.allowForgot = true;
-    this.allowToken = config.get('ALLOW_TOKEN', false);
-  }
-
-  ngOnInit() {
-    this.grange.core.auth.getInfo().subscribe(
-      res => {
-        this.allowRegister = res.register;
-        if (res.social !== undefined && res.social.length > 0) {
-          this.allowSocial = true;
-          this.providers = res.social;
-        }
-        this.title = res.title;
-        markForCheck(this.cdr);
-      }
-    );
-    this.grange.core.auth.isAuthenticated.subscribe(auth => {
-      this.isLogged = auth.state;
-      markForCheck(this.cdr);
-    });
-  }
-
-  onRegister() {
-    this.grange.traverser.traverse('/@@register');
-  }
-
-  onForgot() {
-    this.grange.traverser.traverse('/@@forgot');
-  }
-
-  socialLogin(provider: string) {
-    this.grange.core.auth.goSocialLogin(provider, '@@callback');
-  }
-
-  onSubmit(data: LoginInfo) {
-    this.error = '';
-    if (!this.useToken) {
-      const info = data as LoginInfo;
-      this.grange.core.auth.login(info.login, info.password).subscribe(isLogged => {
-        if (isLogged) {
-          this.grange.traverser.traverse('/');
-        }
-      });
-    } else if (!!data.token) {
-      this.grange.core.auth.setAuthToken(data.token);
-      this.grange.traverser.traverse('/');
+    constructor(
+        public grange: Grange,
+        private cdr: ChangeDetectorRef
+    ) {
+        this.allowForgot = true;
+        this.allowToken = this.grange.core.config.get('ALLOW_TOKEN', false);
+        this.hasSocialLogin = this.grange.core.config.get('SOCIAL_LOGIN', false);
     }
-  }
+
+    ngOnInit() {
+        this.grange.core.auth.getInfo().subscribe(
+            res => {
+                this.allowRegister = res.register;
+                if (res.social !== undefined && res.social.length > 0) {
+                    this.allowSocial = true;
+                    this.providers = res.social;
+                }
+                this.title = res.title;
+                markForCheck(this.cdr);
+            }
+        );
+        this.grange.core.auth.isAuthenticated.subscribe(auth => {
+            this.isLogged = auth.state;
+            markForCheck(this.cdr);
+        });
+    }
+
+    onRegister() {
+        this.grange.traverser.traverse('/@@register');
+    }
+
+    onForgot() {
+        this.grange.traverser.traverse('/@@forgot');
+    }
+
+    socialLogin(provider: string) {
+        this.grange.core.auth.goSocialLogin(provider, '@@callback');
+    }
+
+    onSubmit(data: LoginInfo) {
+        this.error = '';
+        if (!this.useToken) {
+            const info = data as LoginInfo;
+            this.grange.core.auth.login(info.login, info.password).subscribe(isLogged => {
+                if (isLogged) {
+                    this.grange.traverser.traverse('/');
+                }
+            });
+        } else if (!!data.token) {
+            this.grange.core.auth.setAuthToken(data.token);
+            this.grange.traverser.traverse('/');
+        }
+    }
 }
